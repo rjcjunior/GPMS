bc21f8820ac8334930865e7f9b7298c36845a5a7
Ricardo Junior <rjcjunior@hotmail.com>
2017-10-01 09:51:07 +0000

    WooCommerce

---
+++ wp-content/themes/hestia-pro.1.1.43/hestia-pro/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "wrapper" div and all content after.
+ *
+ * @package Hestia
+ * @since Hestia 1.0
+ */
+?>
+			<footer>
+				<div class="container" >
+		 			<div class="row">
+						<a class="footer-social" href="www.github.com.br">
+							<i class="fa fa-github" aria-hidden="true"></i>
+						</a>
+						<h4 class="footer-descricao">Site desenvolvido para fins academicos. </h4>
+					</div>
+			    </div>
+			</footer>
+		</div>
+	</div>
+<?php wp_footer(); ?>
+</body>
+</html>
