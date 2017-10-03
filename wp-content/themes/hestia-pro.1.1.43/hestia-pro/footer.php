c4fb3bb72756250645acb4d80ca7820666eaf86c
Ricardo Junior <rjcjunior@hotmail.com>
2017-10-03 00:01:57 +0000

    Alteraçã Footer

--- wp-content/themes/hestia-pro.1.1.43/hestia-pro/footer.php
+++ wp-content/themes/hestia-pro.1.1.43/hestia-pro/footer.php
@@ -11,7 +11,7 @@
 			<footer>
 				<div class="container" >
 		 			<div class="row">
-						<a class="footer-social" href="www.github.com.br">
+						<a class="footer-social" href="https://github.com/rjcjunior/GPMS" target="_blank">
 							<i class="fa fa-github" aria-hidden="true"></i>
 						</a>
 						<h4 class="footer-descricao">Site desenvolvido para fins academicos. </h4>
